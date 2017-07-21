e9172ccbee59eeb802eb0a6f5e72ef6ae233e2cf
Isshu Rakusai <rakusai@kamilabo.jp>
2017-07-21 12:22:14 +0900

    gulpを整理

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,6 @@ const iconfont = require('gulp-iconfont')
 const iconfontCss = require('gulp-iconfont-css')
 const replace = require('gulp-replace')
 const glob = require('glob-promise')
-const md5 = require('MD5')
-const md5File = require('md5-file/promise')
 
 const development = environments.development
 const production = environments.production
@@ -65,60 +63,10 @@ gulp.task('iconfont', function () {
     .pipe(gulp.dest('./public/fonts/'))
 })
 
-function getAssetList () {
-  return glob('./public/**/*.*').then(files => {
-    files = files.map(file => file.replace(/^\.\/public\//, '/'))
-    files.push('/app.html')
-    files.push('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css')
-    return files
-  })
-}
-
-function getAssetHash () {
-  let tmpFiles
-  return glob('./src/client/**/*.*')
-    .then(files => {
-      tmpFiles = files
-      // sw.jsはpublicにあるのものはhashを含んでいるのでclientのものを使う
-      //let index = tmpFiles.indexOf('./public/sw.js')
-      //if (index !== false) {
-      //  tmpFiles.splice(index, 1);
-      //}
-      //tmpFiles.push('./src/client/js/sw.js')
-      return glob('./views/**/*.*')
-    })
-    .then(files => {
-      files = files.concat(tmpFiles)
-      return files
-    }).then(files => {
-      console.log(files)
-      return Promise.all(files.map(file => {
-        return md5File(file)
-      }))
-    }).then(results => {
-      console.log(results)
-      let all = results.join(',')
-      return md5(all)
-    })
-}
-
-
 // service workerのASSETSとCHECKSUMを作ってsw.jsを作成
 gulp.task('serviceworker', function () {
-  let checksum
-
-  return getAssetHash()
-    .then(result => {
-      console.log(result)
-      checksum = result
-      return getAssetList()
-    })
-    .then(files => {
-      return gulp.src(['./src/client/js/sw.js'])
-        .pipe(replace(/ASSETS = .*/, `ASSETS = [\n'${files.join('\',\n\'')}'\n];`))
-        .pipe(replace(/CHECKSUM = .*/, `CHECKSUM = '${checksum}'`))
-        .pipe(gulp.dest('./public/'))
-    })
+  return gulp.src(['./src/client/js/sw.js'])
+    .pipe(gulp.dest('./public/'))
 })
 
 // ファイルの変更を監視する
@@ -126,8 +74,6 @@ gulp.task('serviceworker', function () {
 gulp.task('watch', ['build'], () => {
   gulp.watch('src/client/img/**', ['img'])
   gulp.watch('src/client/fonts/**', ['iconfont'])
-  gulp.watch('public/**', ['serviceworker'])
-  gulp.watch('views/**', ['serviceworker'])
   gulp.watch('src/client/js/sw.js', ['serviceworker'])
 })
 
