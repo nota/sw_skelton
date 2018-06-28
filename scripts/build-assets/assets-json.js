244f503cba51143300a7b873910c52efbe8156c0
Isshu Rakusai <rakusai@kamilabo.jp>
2018-06-28 17:24:05 +0900

    assets.jsonのpathsをurlsに変更

--- scripts/build-assets/assets-json.js
+++ scripts/build-assets/assets-json.js
@@ -5,22 +5,23 @@ const dateFormat = require('dateformat')
 const now = new Date()
 const version = 'assets-' + dateFormat(now, 'yyyymmdd-HHMMss')
 
-const paths = [
-  '/app.html',
-  '/index.js',
-  'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
-  'https://fonts.googleapis.com/css?family=Open+Sans:400,700'
-]
 const pattern = /\.(woff2|css|png|jpg|gif|svg|ico|js)$/
 
-const files = execFileSync('find', [ './public/assets' ])
+const paths = execFileSync('find', [ './public/assets' ])
                 .toString()
                 .split('\n')
                 .filter(file => file.match(pattern))
                 .map(file => file.replace('./public', ''))
-paths.push(...files)
 
-const body = JSON.stringify({version, paths}, null, 2)
+const urls = [
+  '/app.html',
+  '/index.js',
+  'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
+  'https://fonts.googleapis.com/css?family=Open+Sans:400,700',
+  ...paths
+]
+
+const body = JSON.stringify({version, urls}, null, 2)
 
 const dir = './public/assets'
 if (!fs.existsSync(dir)) fs.mkdirSync(dir)
