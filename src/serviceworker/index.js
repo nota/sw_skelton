817bc04847c3b24a1a9ac0176ec9760d8f3e2ca5
Sho Hashimoto <hashimoto@shokai.org>
2018-07-07 04:02:45 +0900

    service workerのinstall/activate系の処理を src/serviceworker/install/ に移動

--- src/serviceworker/index.js
+++ src/serviceworker/index.js
@@ -1,36 +1,9 @@
 /* eslint-env worker, serviceworker */
 
-import './fetch'
-import {checkForUpdate} from './lib/caches'
+import './fetch/'
+import './install/'
 
 const isDebug = () => location && ['localhost', 'sw-skelton.herokuapp.com'].includes(location.hostname)
 const debug = (...msg) => isDebug() && console.log('%cserviceworker', 'color: gray', ...msg)
 
 debug('start')
-
-self.addEventListener('install', function (event) {
-  debug('install')
-  event.waitUntil(self.skipWaiting())
-})
-
-self.addEventListener('activate', function (event) {
-  debug('activate')
-  event.waitUntil(Promise.all([
-    self.clients.claim(),
-    checkForUpdate()
-  ]))
-})
-
-self.addEventListener('message', function (event) {
-  event.waitUntil(async function () {
-    if (event.data !== 'checkForUpdate') return
-    let ret
-    try {
-      ret = await checkForUpdate()
-    } catch (err) {
-      console.error(err)
-      ret = {error: err.message}
-    }
-    event.ports[0].postMessage(ret)
-  }())
-})
---
+++ src/serviceworker/install/index.js
@@ -0,0 +1,33 @@
+/* eslint-env worker, serviceworker */
+
+import {checkForUpdate} from '../lib/caches'
+
+const isDebug = () => location && ['localhost', 'sw-skelton.herokuapp.com'].includes(location.hostname)
+const debug = (...msg) => isDebug() && console.log('%cserviceworker', 'color: gray', ...msg)
+
+self.addEventListener('install', function (event) {
+  debug('install')
+  event.waitUntil(self.skipWaiting())
+})
+
+self.addEventListener('activate', function (event) {
+  debug('activate')
+  event.waitUntil(Promise.all([
+    self.clients.claim(),
+    checkForUpdate()
+  ]))
+})
+
+self.addEventListener('message', function (event) {
+  event.waitUntil(async function () {
+    if (event.data !== 'checkForUpdate') return
+    let ret
+    try {
+      ret = await checkForUpdate()
+    } catch (err) {
+      console.error(err)
+      ret = {error: err.message}
+    }
+    event.ports[0].postMessage(ret)
+  }())
+})
