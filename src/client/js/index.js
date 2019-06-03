48f8cbafa4c2a29d3e8faf46326322ca20ceaadf
Isshu Rakusai <rakusai@kamilabo.jp>
2019-06-03 16:31:58 +0900

    babel7にアップデート

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -1,6 +1,6 @@
 /* eslint-env browser */
 
-import 'babel-polyfill'
+import '@babel/polyfill'
 import React from 'react'
 import {render} from 'react-dom'
 import App from './components/app'
--- src/serviceworker/index.js
+++ src/serviceworker/index.js
@@ -1,3 +1,4 @@
+import '@babel/polyfill'
 import './fetch/'
 import './install/'
 import './message'
